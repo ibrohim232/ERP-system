c95ade1010fef8bf4e5c8038983ec20a2b6eaa78
ibrohim <nabiyevibrohim232@gmail.com>
2023-10-05 11:23:45 +0500

    js

--- src/main/java/com/example/erpsystem/controller/AuthController.java
+++ src/main/java/com/example/erpsystem/controller/AuthController.java
@@ -1,9 +1,30 @@
 package com.example.erpsystem.controller;
 
 
+import com.example.erpsystem.dto.JwtResponse;
+import com.example.erpsystem.dto.user.SingIdDto;
+import com.example.erpsystem.dto.user.UserRequestDto;
+import com.example.erpsystem.dto.user.UserResponseDto;
+import com.example.erpsystem.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/sing-up")
+    public UserResponseDto singUp(@RequestBody UserRequestDto userRequestDto) {
+        return userService.create(userRequestDto);
+    }
+
+    @PostMapping("sing-in")
+    public JwtResponse singIn(@RequestBody SingIdDto singIdDto) {
+        return userService.singIn(singIdDto);
+    }
+    
 
 }
