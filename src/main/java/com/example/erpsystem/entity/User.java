478cb9def5a0741d4525c5743e44c3a45036fdcb
ibrohim <nabiyevibrohim232@gmail.com>
2023-10-05 09:10:01 +0500

    base

---
+++ src/main/java/com/example/erpsystem/entity/User.java
@@ -0,0 +1,15 @@
+package com.example.erpsystem.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User extends BaseEntity{
+}
